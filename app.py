cc1cc8e80d9dd8fb2b485ea5c9fef806c184283f
Ward De Ridder <ward.deridder@ap.be>
2021-12-07 14:40:09 +0000

    add ua-bike

--- app.py
+++ app.py
@@ -39,6 +39,42 @@ def api_helium():
       mysql.get_db().commit()
   return "Done"
 
+@app.route("/api/ttn", methods = ['POST']) #voor UA
+def api_ttn():
+  content = request.json
+  conn = mysql.connect()
+  cursor = conn.cursor()
+  dev_eui = content["end_device_ids"]["dev_eui"]
+  payload = content["uplink_message"]["frm_payload"]
+  latitude = float(content["uplink_message"]["decoded_payload"]["latitude"])
+  longitude = float(content["uplink_message"]["decoded_payload"]["longitude"])
+  sql = "INSERT INTO bike (id, dev_eui, payload, latitude, longitude) VALUES (Null, %s,%s,%s,%s);"
+  val = (dev_eui, payload, latitude, longitude,)
+  cursor.execute(sql, val)
+  print(cursor._executed)
+  conn.commit()
+  for row in cursor:
+    print(row)        
+  #print(content)
+  return "Done"
+
+@app.route("/api/UA-bike/lastlocation") #voor UA
+def api_UA_lastlocation():
+  features = []
+  conn = mysql.connect()
+  cursor = conn.cursor()
+  eui = request.args.get('dev_eui', default = "Null", type = str)
+  print(eui)
+  cursor.execute("SELECT * FROM bike WHERE dev_eui LIKE %s ORDER BY timestamp DESC LIMIT 1;",(eui))
+  conn.commit()
+  for points in cursor.fetchall():
+    mypoint = Point((points["longitude"], points["latitude"]))
+    headers = CaseInsensitiveDict()
+    headers["User-Agent"] = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0"
+    resp = requests.get("https://nominatim.openstreetmap.org/reverse?format=json&lat="+str(points["latitude"])+"&lon="+str(points["longitude"])+"&zoom=27&addressdetails=1", headers=headers)
+    features.append(Feature(geometry=mypoint, properties={"last-timestamp": points["timestamp"], "adres": resp.json()["display_name"]}))
+  return FeatureCollection(features)
+
 @app.route("/api/geojson/hotspots")
 def api_geojson_hotspots():
   features = []
