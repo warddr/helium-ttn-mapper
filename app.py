cc027c4de345ebba1be6c11937d3752064f20cb7
Ward De Ridder <ward.deridder@ap.be>
2022-01-26 13:25:28 +0100

    cleanup code, only keep relevant Uantwerpen part

--- app.py
+++ app.py
@@ -17,28 +17,6 @@ mysql.init_app(app)
 def hello():
   return "Hello World!"
 
-@app.route("/api/helium", methods = ['POST'])
-def api_helium():
-  content = request.json
-  cursor = mysql.connect().cursor()
-  for hotspot in content["hotspots"]:
-    distance = geopy.distance.distance((hotspot["lat"], hotspot["long"]), (content["decoded"]["payload"]["latitude"],content["decoded"]["payload"]["longitude"])).km
-    cursor = mysql.get_db().cursor()
-    sql = "INSERT INTO heliumtracker (dev_eui, fcnt, frequency, hotspot, rssi, snr, spreading, payload, latitude, longitude, sats, distance) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
-    val = (content["dev_eui"], content["fcnt"], hotspot["frequency"], hotspot["id"], hotspot["rssi"], hotspot["snr"],hotspot["spreading"], content["payload"], content["decoded"]["payload"]["latitude"],content["decoded"]["payload"]["longitude"],content["decoded"]["payload"]["sats"],distance)
-    cursor.execute(sql, val)
-    mysql.get_db().commit()
-
-    cursor2 = mysql.get_db().cursor()
-    cursor2.execute("SELECT * FROM heliumhotspots WHERE id = %s;", hotspot["id"]);
-    if not cursor2.fetchone(): #gateway is not yet in our db
-      cursor3 = mysql.get_db().cursor()
-      sql = "INSERT INTO heliumhotspots (id,name,latitude,longitude) VALUES (%s,%s,%s,%s)"
-      val = (hotspot["id"], hotspot["name"], hotspot["lat"], hotspot["long"])
-      cursor3.execute(sql, val)
-      mysql.get_db().commit()
-  return "Done"
-
 @app.route("/api/ttn", methods = ['POST']) #voor UA
 def api_ttn():
   content = request.json
@@ -77,82 +55,6 @@ def api_UA_lastlocation():
   resp = jsonify(FeatureCollection(features))
   resp.headers['Access-Control-Allow-Origin'] = '*'
   return resp
-
-@app.route("/api/geojson/hotspots")
-def api_geojson_hotspots():
-  features = []
-  cursor = mysql.connect().cursor()
-  eui = request.args.get('dev_eui', default = "%", type = str) #not yet implemented
-  hotspot = request.args.get('hotspot', default = "%", type = str)
-  cursor.execute("SELECT * FROM heliumhotspots WHERE id LIKE %s;",(hotspot))
-  for points in cursor.fetchall():
-    mypoint = Point((points["longitude"], points["latitude"]))
-    features.append(Feature(geometry=mypoint, properties={"name": points["name"], "id": points["id"]}))
-  return FeatureCollection(features)
-
-@app.route("/api/geojson/points")
-def api_geojson_points():
-  features = []
-  cursor = mysql.connect().cursor()
-  eui = request.args.get('dev_eui', default = "%", type = str)
-  hotspot = request.args.get('hotspot', default = "%", type = str)
-  cursor.execute("SELECT fcnt, round(heliumtracker.longitude,3) as longitude, round(heliumtracker.latitude,3) as latitude, heliumhotspots.longitude, heliumhotspots.latitude, rssi, distance, hotspot FROM heliumtracker INNER JOIN heliumhotspots ON heliumtracker.hotspot = heliumhotspots.id WHERE dev_eui LIKE %s AND hotspot LIKE %s ORDER BY latitude, longitude, hotspot, rssi DESC;", (eui, hotspot))
-  lastlat = 0
-  lastlong = 0
-  lasthotspot = ""
-  for points in cursor.fetchall():
-    if not((points["longitude"] == lastlong) and (points["latitude"] == lastlat)): #only 1 point if received by multiple gateways
-      mypoint = Point((points["longitude"], points["latitude"]))
-      features.append(Feature(geometry=mypoint))
-      lasthotspot = ""
-    lastlat = points["latitude"]
-    lasthotspot = points["hotspot"]
-    lastlong = points["longitude"]
-  return FeatureCollection(features)
-
-@app.route("/api/geojson/lines")
-def api_geojson_lines():
-  features = []
-  cursor = mysql.connect().cursor()
-  eui = request.args.get('dev_eui', default = "%", type = str)
-  hotspot = request.args.get('hotspot', default = "%", type = str)
-  cursor.execute("SELECT fcnt, round(heliumtracker.longitude,3) as longitude, round(heliumtracker.latitude,3) as latitude, heliumhotspots.longitude, heliumhotspots.latitude, rssi, distance, hotspot FROM heliumtracker INNER JOIN heliumhotspots ON heliumtracker.hotspot = heliumhotspots.id WHERE dev_eui LIKE %s AND hotspot LIKE %s ORDER BY latitude, longitude, hotspot, rssi DESC;", (eui, hotspot))
-  lastlat = 0
-  lastlong = 0
-  lasthotspot = ""
-  for points in cursor.fetchall():
-    if not((points["longitude"] == lastlong) and (points["latitude"] == lastlat)): #only 1 point if received by multiple gateways
-      lasthotspot = ""
-    if (points["hotspot"] != lasthotspot):
-      myline = LineString([(points["longitude"], points["latitude"]),(points["heliumhotspots.longitude"], points["heliumhotspots.latitude"])])
-      features.append(Feature(geometry=myline, properties={"rssi": points["rssi"]}))
-    lastlat = points["latitude"]
-    lasthotspot = points["hotspot"]
-    lastlong = points["longitude"]
-  return FeatureCollection(features)
-
-@app.route("/highscore")
-def highscore():
-  cursor = mysql.get_db().cursor()
-  cursor.execute("SELECT dev_eui, max(distance) as distance FROM heliumtracker GROUP BY dev_eui ORDER BY distance DESC;")
-  
-  return render_template('highscore.html.j2', scores = cursor.fetchall())
-
-@app.route("/map")
-def map():
-  eui = request.args.get('dev_eui', default = "%25", type = str)
-  hotspot = request.args.get('hotspot', default = "%25", type = str)
-  return render_template('map.html.j2', hotspots="api/geojson/hotspots?dev_eui="+eui+"&hotspot="+hotspot, points="api/geojson/points?dev_eui="+eui+"&hotspot="+hotspot, lines="api/geojson/lines?dev_eui="+eui+"&hotspot="+hotspot)
-
-@app.route("/waarisward")
-def waarisward():
-  cursor = mysql.connect().cursor()
-  cursor.execute("SELECT *  FROM heliumtracker WHERE dev_eui = '6081F9D6C064A43C' ORDER BY timestamp  DESC LIMIT 1;")
-  point = cursor.fetchone()
-  headers = CaseInsensitiveDict()
-  headers["User-Agent"] = "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:89.0) Gecko/20100101 Firefox/89.0"
-  resp = requests.get("https://nominatim.openstreetmap.org/reverse?format=json&lat="+str(point["latitude"])+"&lon="+str(point["longitude"])+"&zoom=27&addressdetails=1", headers=headers)
-  return (resp.json()["display_name"])
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
--- database.sql
+++ database.sql
@@ -1,25 +1,31 @@
-CREATE TABLE `heliumhotspots` (
-  `id` varchar(65) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `latitude` double NOT NULL,
-  `longitude` double NOT NULL,
-  `access_time` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-
-CREATE TABLE `heliumtracker` (
+CREATE TABLE `bike` (
   `id` int(11) NOT NULL,
-  `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
   `dev_eui` varchar(20) NOT NULL,
-  `fcnt` int(11) NOT NULL,
-  `frequency` float NOT NULL,
-  `hotspot` varchar(65) NOT NULL,
-  `rssi` int(11) NOT NULL,
-  `snr` int(11) NOT NULL,
-  `spreading` varchar(20) NOT NULL,
   `payload` varchar(50) NOT NULL,
   `latitude` double NOT NULL,
   `longitude` double NOT NULL,
-  `sats` tinyint(4) NOT NULL,
-  `distance` float NOT NULL
+  `timestamp` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Indexes for table `bike`
+--
+ALTER TABLE `bike`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `bike`
+--
+ALTER TABLE `bike`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
